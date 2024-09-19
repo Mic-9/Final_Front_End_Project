c5a3147d8b50e94dbe40edf1e0a34b8bb9cb6208
Mic-9 <116760683+Mic-9@users.noreply.github.com>
2024-09-19 22:37:25 +0200

    Initial project setup

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import linkedin from "../../img/linkedin-icon.svg";
+import github from "../../img/logo-github.svg";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="Footer" id="footer">
+      <div className="F-container">
+        <div className="F-item">Michele Maioli &copy; 2024</div>
+        <div className="F-item">
+          <a
+            id="git"
+            href="https://github.com/Mic-9"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={github} alt="GitHub Logo" />
+          </a>
+          <a
+            id="link"
+            href="https://www.linkedin.com/in/michelemaioli"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedin} alt="LinkedIn Icon" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
